e36cd186d3065cb5a66e9cee700709b6c9d9826c
dance2die <dontbothermewithspam@gmail.com>
2015-04-26 21:31:13 -0400

    initial import

---
+++ Demo.Exif.ShowData/Demo.Exif.ShowData.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace Demo.Exif.ShowData.Console
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var files = GetFiles();
+			foreach (var file in files)
+			{
+				System.Console.WriteLine("========== File: {0} ==========", file);
+				DisplayFileExif(file);
+			}
+		}
+
+		private static void DisplayFileExif(string file)
+		{
+			using (var image = Image.FromFile(file))
+			{
+				var exif = new ExifNET.Exif(image.PropertyItems);
+				DisplayExif(exif);
+			}
+		}
+
+		private static void DisplayExif(ExifNET.Exif exif)
+		{
+			foreach (var property in exif.GetType().GetProperties())
+			{
+				System.Console.WriteLine("\t{0}={1}", property.Name, property.GetValue(exif, null));
+			}
+		}
+
+		private static IEnumerable<string> GetFiles()
+		{
+			return Directory.EnumerateFiles(@".\Contents", "*.jpg", SearchOption.TopDirectoryOnly);
+		}
+	}
+}
